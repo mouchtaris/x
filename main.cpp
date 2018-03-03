91bfd850008851b697ca90ca551aaf7375994e45
Nikos Mouchtaris <Nikos Mouchtaris mouchtaris@gmail.com>
2018-03-03 15:14:29 +0000

    No range

--- main.cpp
+++ main.cpp
@@ -9,25 +9,10 @@
 #include "next_numeric.hpp"
 #include "range.hpp"
 
-struct Source {
+template <typename T>
+struct Source;
 
-    const int begin = 2;
-    const int last = 5;
-    std::optional<int> current = begin;
-
-    std::optional<int> pull() {
-        if (current.has_value()) {
-            auto result = std::move(current);
-            current = next(current.value());
-            return result;
-        }
-        return current;
-    }
-
-};
 
 int main(int, char**) {
-    Source s;
-    range(2, 5, [](int n) { std::cout << n << '\n'; });
     return 0;
 }
--- src/range.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-template <typename T, typename C>
-void range(T from, T to, C callback);
