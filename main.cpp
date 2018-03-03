2b39daa6052649aa51fb4dd85f0eefd76b959dab
Nikos Mouchtaris <Nikos Mouchtaris mouchtaris@gmail.com>
2018-03-03 15:36:50 +0000

    Funky range

--- main.cpp
+++ main.cpp
@@ -7,12 +7,49 @@
 
 #include "equal_numeric.hpp"
 #include "next_numeric.hpp"
-#include "range.hpp"
 
 template <typename T>
-struct Source;
+struct Range;
 
+template <typename T, typename C>
+void range(T self, C callback)
+{
+    Range<T> { }(std::move(self), std::move(callback));
+}
+
+template <typename T>
+struct Range<std::tuple<T, T>> {
+
+    using Self = std::tuple<T, T>;
+
+    template <typename C>
+    void operator()(const Self& self, const C callback) const {
+        auto&& from = std::get<0>(self);
+        auto&& to = std::get<1>(self);
+
+        callback(from);
+
+        if (equal(from, to))
+            return;
+
+        std::optional<T> maybeNext = next(std::move(from));
+
+        if (maybeNext.has_value()) {
+            Self nextSelf = {
+                std::move(maybeNext.value()),
+                std::move(to)
+            };
+            (*this)(std::move(nextSelf), std::move(callback));
+        }
+    }
+
+};
 
 int main(int, char**) {
+    auto println = [](auto&& any) { std::cout << any << '\n'; };
+    // range(std::make_tuple(2, 5), println);
+    std::tuple<int, int> self = { 2, 5 };
+    Range<std::tuple<int, int>> r;
+    r(self, println);
     return 0;
 }
