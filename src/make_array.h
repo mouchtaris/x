094f67a693ff5425b7bd8c6fd2a82c77a685f168
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2018-05-05 09:19:22 +0000

    Add make_array()

---
+++ src/make_array.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <array>
+
+template <
+    std::size_t ...n,
+    typename C>
+constexpr auto __make_array__impl(
+    std::index_sequence<n...>,
+    C&& construct
+) 
+    ->  std::array<
+            std::invoke_result_t<C, std::size_t>,
+            sizeof...(n)>
+{
+    return { construct(n) ... , };
+}
+
+template <
+    std::size_t n,
+    typename C>
+constexpr auto make_array(
+    C&& construct
+)
+{
+    return __make_array__impl(
+        std::make_index_sequence<n>(),
+        std::forward<C>(construct)
+    );
+}
